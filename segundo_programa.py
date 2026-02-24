e2077038a57f3cfe23ede14793918d1211c6232f
videiravierarenan-max <you@example.com>
2026-02-24 08:52:31 -0300

    exercicio df

---
+++ segundo_programa.py
@@ -0,0 +1,8 @@
+import pandas as pd
+df = pd.read_csv("C:\\Users\\videi\\OneDrive\\Documents\\analisededados-renan\\notas.csv")
+df.shape
+df.columns
+df.dtypes
+df.isna().sum()
+df.head()
+df.tail()
