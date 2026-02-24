4bc43555eb2751767c37dda4a9c7c16bb602a728
videiravierarenan-max <you@example.com>
2026-02-24 08:09:53 -0300

    lista 1

--- Primeiro_progrma.py
+++ Primeiro_progrma.py
@@ -1,5 +1,93 @@
 print ("hello word")
 1+1
+lista=[objeto,idade,x]
+lista[2]
+
+print(lista)
+lista_mista= ["cavalo","pato",1,2,3]
+print(lista_mista)
+
+for item in lista_mista:
+  if type(item)== str:
+    animal.append(item)
+    elif type (item)== int:
+numero.append(item)
+animal[0]
+while item == True:
+  print(pare encontre a string)
+  item = input("Digite uma string: ")
+dict_mista={p,1:"cavalo",2:"pato",3:1,4:2,5:3}
+print(dict_mista)
+
+#exercicios 24
+lista2=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+for item in lista2:
+  if item % 2 == 0:
+    print(f"{item} é par")
+  else:
+    print(f"{item} é impar")
+#exercicio 25
+lista3=list(range(1,21))
+print(lista3)
+for item in lista3:
+  quadrado=item**2
+#exercicio 26
+lista4=("python", "java", "c", "javascript")
+for item in lista4:
+  numero_letras=len(item)
+print(f"{item} tem {numero_letras} letras")
+#exercicio 27
+lista5=[12, 18, 25, 40, 60]
+for item in lista5:
+  if item >= 18:
+    print(f"{item} é maior de idade")
+  else:
+    print(f"{item} é menor de idade")
+    #exrcicio28 
+   lista6 = [5.5, 7.0, 8.3, 4.9, 6.2]
+  for item in lista6:
+    if item >= 7.0:
+      print(f"{item} é aprovado")
+    else:
+      print(f"{item} é reprovado")
+#exercicio 29
+lista7 = [["arroz", "feijão", "batata", "carne"]]
+for item in lista7:
+  print(f"preciso comprar {item}")  
+  #30 exercicio
+  i = 1
+while i <= 10:
+    print(i)
+    i += 1
+    #exercicio 31
+    num = int(input("Digite um número inteiro (0 para sair): "))
+while num != 0:
+    print("Você digitou:", num)
+    num = int(input("Digite um número inteiro (0 para sair): "))
+    #exercicio 32
+    i = 1
+soma = 0
+while i <= 100:
+    soma += i
+    i += 1
+print("A soma de 1 a 100 é:", soma)
+#exercicio 33
+secreto = 7
+palpite = int(input("Adivinhe o número secreto: "))
+while palpite != secreto:
+    print("Errado, tente novamente!")
+    palpite = int(input("Adivinhe o número secreto: "))
+print("Parabéns! Você acertou!")
+#EXERCICIO 34
+i = 2
+while i <= 20:
+    print(i)
+    i += 2
+
+
+
+
+
 
 
 
